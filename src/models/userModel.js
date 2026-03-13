9a1c87fdd71e75467a6b8baa8a7355d0156fb4c0
Rodrigo <shaunkiplays@gmail.com>
2026-03-13 19:04:27 -0300

    feat: add userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,21 @@
+const pool = require('../config/db');
+
+exports.createUser = async (name, email, password) => {
+  const result = await pool.query(
+    `INSERT INTO users (name, email, password)
+     VALUES ($1,$2,$3)
+     RETURNING id, name, email`,
+    [name, email, password]
+  );
+
+  return result.rows[0];
+};
+
+exports.findByEmail = async (email) => {
+  const result = await pool.query(
+    'SELECT * FROM users WHERE email = $1',
+    [email]
+  );
+
+  return result.rows[0];
+};
